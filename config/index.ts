6f40b4c4fcdb80e3bc66f765450607dfa594a8f0
sorphwer <sorphwer@gmail.com>
2024-08-09 22:08:54 +0800

    update config

--- config/index.ts
+++ config/index.ts
@@ -3,10 +3,10 @@ export const APP_ID = `${process.env.NEXT_PUBLIC_APP_ID}`
 export const API_KEY = `${process.env.NEXT_PUBLIC_APP_KEY}`
 export const API_URL = `${process.env.NEXT_PUBLIC_API_URL}`
 export const APP_INFO: AppInfo = {
-  title: 'Chat APP',
-  description: '',
-  copyright: '',
-  privacy_policy: '',
+  title: 'RiinoSite Agent',
+  description: 'Des',
+  copyright: 'RiinoSite',
+  privacy_policy: 'https://riino.site/privacy_statement/',
   default_language: 'en',
 }
 
